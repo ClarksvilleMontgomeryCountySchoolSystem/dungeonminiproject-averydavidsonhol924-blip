0fe0c2d824d246e33a4accf39b74ddf7c9b66d0b
avery H. <avery.davidsonhol924@students.cmcss.net>
2026-03-04 21:22:06 -0600

    yello

---
+++ task3.py
@@ -0,0 +1,6 @@
+guard_awake = False
+if not guard_awake:
+    outcome = ("Shadow: I have to this quickly and quiet to make sure i dont wake the guard")
+else:
+    outcome = ("Doom: NO NO NO PLEASE DONT HURT ME I WONT DO IT AGAIN IM SORRY")
+print(outcome)
---
+++ task5.py
@@ -0,0 +1,6 @@
+escaped = False
+if not escaped:
+    outcome = ("Doom: NOOOOOOOO I WASTED ALL THIS TIME I DIDNT ESCAPE.")
+else:
+    outcome = ("Legend: YESSSSSSSSS I MADEEEE IT HOMEEE BABY WOOOHOOOO")
+print(outcome)
